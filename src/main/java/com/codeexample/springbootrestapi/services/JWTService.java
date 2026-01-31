e58e9d6506c81937e8bb384f2c3954a5df476273
AamirrH <aamirr.1704@gmail.com>
2026-01-31 12:12:54 +0530

    Add JWTService, generateToken method and getPayload method

---
+++ src/main/java/com/codeexample/springbootrestapi/services/JWTService.java
@@ -0,0 +1,47 @@
+package com.codeexample.springbootrestapi.services;
+
+
+import com.codeexample.springbootrestapi.entities.UserEntity;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+
+@Service
+public class JWTService {
+
+    @Value("${jwt.secretkey}")
+    private String secretKey;
+
+    private SecretKey getsecretKey(){
+        return Keys.hmacShaKeyFor(secretKey.getBytes(StandardCharsets.UTF_8));
+    }
+
+
+    public String generateToken(UserEntity userEntity){
+        return Jwts.builder()
+                .subject(userEntity.getId().toString())
+                .claim("username",userEntity.getUsername())
+                .claim("roles",userEntity.getRoles())
+                .issuedAt(new Date())
+                .expiration(new Date(System.currentTimeMillis() + 1000*120))
+                .signWith(getsecretKey())
+                .compact();
+    }
+    public Long getUserIdFromToken(String token){
+        Claims claim = Jwts.parser()
+                .verifyWith(getsecretKey())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+        return Long.valueOf(claim.getSubject());
+
+    }
+
+
+}
--- src/test/java/com/codeexample/springbootrestapi/SpringBootRestApiApplicationTests.java
+++ src/test/java/com/codeexample/springbootrestapi/SpringBootRestApiApplicationTests.java
@@ -1,13 +1,32 @@
 package com.codeexample.springbootrestapi;
 
+import com.codeexample.springbootrestapi.entities.UserEntity;
+import com.codeexample.springbootrestapi.services.JWTService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SpringBootRestApiApplicationTests {
 
+    @Autowired
+    private JWTService jwtService;
+
     @Test
     void contextLoads() {
+
+        UserEntity user = new UserEntity();
+        user.setId(44L);
+        user.setUsername("username1");
+        user.setPassword("password1");
+
+        String tokenReturned = jwtService.generateToken(user);
+        Long idReturned = jwtService.getUserIdFromToken(tokenReturned);
+        System.out.println("The token returned is: " + tokenReturned);
+        System.out.println("The id from payload returned is: " + idReturned);
+
     }
+
+
 
 }
